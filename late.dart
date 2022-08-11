01f77b68a215265d94472c8ec96438bee15f4855
thuanmystic <2021050033@student.humg.edu.vn>
2022-08-11 07:50:26 +0700

    2

---
+++ late.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  //late khi khai bao thi se dc gan gia tri vao cho bien truoc khi chuong trinh dc chay
+
+  late int a;
+  //thuc hien gan gia tri cho bien a truoc khi chay chuong trinh neu ko gan chuong trinh se bi loi
+  a = 10;
+  print('So a dc gan la: $a');
+}
