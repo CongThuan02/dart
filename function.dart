c55a55e46f9d22f3b5f95fe8b9bc32d108515ff2
thuanmystic <2021050033@student.humg.edu.vn>
2022-08-11 08:50:08 +0700

    2

--- function.dart
+++ function.dart
@@ -1,13 +1,16 @@
 import 'dart:ffi';
 
 //hàm thông thường
+//bat buoc phai co day du tham so chuyen vao
 int tinhTong(int a, int b, int c) => a + b + c;
 
-//ham voi tham so mac dinh
 
+//ham voi tham so mac dinh
+//da co tham so mac dinh duoc dat trong cap dau {} chi can chuyen vao cac tham so con thieu
 double tinhTong2(var a, {double b: 2, double c: 1}) => a + b + c;
 
 //ham voi tham so tuy chon
+// cho phep ham cho tham so tuy chon chuyen vao duoc dat trong cap dau [] neu nhu khong co tham so chuyen vao thi gia tri cua tham so trong cap dau [] la null 
 int? tinhTong3(int a, [int? b, int? c]) {
   if (b == null && c == null) {
     return a;
